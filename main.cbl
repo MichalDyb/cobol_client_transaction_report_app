6bffb71fad4f8c3254156e036fb3b497f6224782
MichalDyb <michal.dyb@interia.pl>
2022-10-03 22:29:51 +0200

    End of project

--- main.cbl
+++ main.cbl
@@ -8,16 +8,15 @@
            INPUT-OUTPUT SECTION.
                FILE-CONTROL.
       *******************************         
-               SELECT CLIENT ASSIGN TO 'clients.dat'
+               SELECT CLIENT ASSIGN TO 'clients.txt'
                FILE STATUS CLIENT-FILE-STATUS
                ORGANIZATION IS LINE SEQUENTIAL.
       *******************************
-               SELECT TRANSACTION ASSIGN TO 'clients-transactions.dat'
+               SELECT TRANSACTION ASSIGN TO 'clients-transactions.txt'
                FILE STATUS TRANSACTION-FILE-STATUS
                ORGANIZATION IS LINE SEQUENTIAL.
       *******************************         
-               SELECT RAPORT ASSIGN TO 'report.dat'
-               ORGANIZATION IS LINE SEQUENTIAL.
+               SELECT RAPORT ASSIGN TO 'report.txt'.
 
       *******************************
        DATA DIVISION.
@@ -38,22 +37,12 @@
                05 TR-TYPE PIC A(1).
                05 TR-AMOUNT PIC 9(6)V9(2).
       *******************************
-           FD RAPORT RECORDING MODE F.
+           FD RAPORT 
+               RECORD CONTAINS 80 CHARACTERS
+               RECORDING MODE F
+               DATA RECORD IS RAPORT-FILE.
            01 RAPORT-FILE.
-               05 FILLER PIC X(2) VALUE SPACES.
-               05 RA-TR-COUNTRY-CODE PIC A(2).
-               05 FILLER PIC X(2) VALUE SPACES.
-               05 RA-CL-ID PIC 9(3).
-               05 FILLER PIC X(2) VALUE SPACES.
-               05 RA-CL-NAME PIC A(20).
-               05 FILLER PIC X(2) VALUE SPACES.
-               05 RA-CL-ADDRESS PIC X(20).
-               05 FILLER PIC X(2) VALUE SPACES.
-               05 RA-CL-NIP PIC 9(10).
-               05 RA-TR-SUMMARY OCCURS 10 TIMES.
-                   10 RA-TR-CURRENCY PIC A(3).
-                   10 RA-TR-DEBIT-SUM PIC 9(6)V9(2).
-                   10 RA-TR-CREDIT-SUM PIC 9(6)V9(2).
+               05 WS-DECORATOR PIC X(80) VALUE SPACES.
 
            WORKING-STORAGE SECTION.
       *******************************
@@ -70,6 +59,18 @@
                05 WS-TR-TYPE PIC A(1).
                05 WS-TR-AMOUNT PIC 9(6)V9(2).
            
+           01 WS-RAPORT.
+               05 RA-CLI-INFO.
+                   10 RA-CL-ID PIC 9(3).
+                   10 RA-CL-NAME PIC A(20).
+                   10 RA-CL-ADDRESS PIC X(20).
+                   10 RA-CL-NIP PIC 9(10).
+                   10 RA-TR-COUNTRY-CODE PIC A(2).
+               05 RA-TR-SUMMARY OCCURS 10 TIMES.
+                   10 RA-TR-CURRENCY PIC A(3).
+                   10 RA-TR-DEBIT-SUM PIC 9(6)v9(2).
+                   10 RA-TR-CREDIT-SUM PIC 9(6)V9(2).
+
       *******************************
            01 WS-COUNTRY-CODE PIC A(2).
       *******************************
@@ -80,6 +81,45 @@
       *******************************
            01 TRANSACTION-FILE-STATUS PIC X(2).
            01 WS-TR-RECORD-FOUND PIC A(1) VALUE 'N'.
+
+      *******************************
+           01 WS-FIRST-REPORT-LINE PIC A(1) VALUE 'Y'.
+
+      *******************************
+           01 WS-HEADER-1.
+               05 FILLER PIC X(10) VALUE '----------' OCCURS 8.
+      
+      *******************************
+           01 WS-HEADER-2.
+               05 FILLER PIC X(26) VALUE '-- '. 
+               05 FILLER PIC X(52) VALUE 'Clients Transactions Report'. 
+               05 FILLER PIC X(2) VALUE '--'.    
+
+      *******************************
+           01 WS-HEADER-3.
+               05 FILLER PIC X(6) VALUE '-- '. 
+               05 HE-CL-ID PIC 9(4).
+               05 FILLER PIC X(3) VALUE SPACES. 
+               05 HE-CL-NAME PIC A(23).
+               05 HE-CL-ADDRESS PIC X(20).
+               05 FILLER PIC X(3) VALUE SPACES. 
+               05 HE-CL-NIP PIC 9(10).
+               05 FILLER PIC X(3) VALUE SPACES. 
+               05 HE-TR-COUNTRY-CODE PIC A(5).
+               05 FILLER PIC X(3) VALUE ' --'. 
+      
+      *******************************
+           01 WS-HEADER-4.
+               05 FILLER PIC X(28) VALUE '-- '.
+               05 FILLER PIC X(6) VALUE 'Debit '.
+               05 HE-TR-DEBIT-SUM PIC 9(6)v9(2).
+               05 FILLER PIC X(1) VALUE SPACE.  
+               05 HE-TR-CURRENCY-1 PIC A(12).
+               05 FILLER PIC X(7) VALUE 'Credit '.  
+               05 HE-TR-CREDIT-SUM PIC 9(6)V9(2).
+               05 FILLER PIC X(1) VALUE SPACE. 
+               05 HE-TR-CURRENCY-2 PIC A(6).
+               05 FILLER PIC X(3) VALUE ' --'. 
 
       *******************************
        PROCEDURE DIVISION.
@@ -212,9 +252,44 @@
 
       *******************************
        SAVE-RAPORT-NEXT-LINE-PARA.
-       WRITE RAPORT-FILE
-       END-WRITE.
-       MOVE 'N' TO WS-CL-RECORD-FOUND.
+       IF WS-FIRST-REPORT-LINE = 'Y' THEN
+           MOVE 'N' TO WS-FIRST-REPORT-LINE
+           WRITE RAPORT-FILE FROM WS-HEADER-1
+           WRITE RAPORT-FILE FROM WS-HEADER-2 AFTER ADVANCING 1 LINE
+           WRITE RAPORT-FILE FROM WS-HEADER-1 AFTER ADVANCING 1 LINE
+           MOVE RA-CL-ID TO HE-CL-ID
+           MOVE RA-CL-NAME TO HE-CL-NAME
+           MOVE RA-CL-ADDRESS TO HE-CL-ADDRESS
+           MOVE RA-CL-NIP TO HE-CL-NIP
+           MOVE RA-TR-COUNTRY-CODE TO HE-TR-COUNTRY-CODE
+           WRITE RAPORT-FILE FROM WS-HEADER-3 AFTER ADVANCING 1 LINE
+       PERFORM VARYING WS-A FROM 1 BY 1 UNTIL WS-A = 11
+           MOVE RA-TR-CURRENCY(WS-A) TO HE-TR-CURRENCY-1
+           MOVE RA-TR-CURRENCY(WS-A) TO HE-TR-CURRENCY-2
+           MOVE RA-TR-DEBIT-SUM(WS-A) TO HE-TR-DEBIT-SUM
+           MOVE RA-TR-CREDIT-SUM(WS-A) TO HE-TR-CREDIT-SUM
+           WRITE RAPORT-FILE FROM WS-HEADER-4 AFTER ADVANCING 1 LINE
+       END-PERFORM
+           WRITE RAPORT-FILE FROM WS-HEADER-1 AFTER ADVANCING 1 LINE
+           END-WRITE
+       ELSE
+           MOVE RA-CL-ID TO HE-CL-ID
+           MOVE RA-CL-NAME TO HE-CL-NAME
+           MOVE RA-CL-ADDRESS TO HE-CL-ADDRESS
+           MOVE RA-CL-NIP TO HE-CL-NIP
+           MOVE RA-TR-COUNTRY-CODE TO HE-TR-COUNTRY-CODE
+           WRITE RAPORT-FILE FROM WS-HEADER-3 AFTER ADVANCING 1 LINE
+       PERFORM VARYING WS-A FROM 1 BY 1 UNTIL WS-A = 11
+           MOVE RA-TR-CURRENCY(WS-A) TO HE-TR-CURRENCY-1
+           MOVE RA-TR-CURRENCY(WS-A) TO HE-TR-CURRENCY-2
+           MOVE RA-TR-DEBIT-SUM(WS-A) TO HE-TR-DEBIT-SUM
+           MOVE RA-TR-CREDIT-SUM(WS-A) TO HE-TR-CREDIT-SUM
+           WRITE RAPORT-FILE FROM WS-HEADER-4 AFTER ADVANCING 1 LINE
+       END-PERFORM
+           WRITE RAPORT-FILE FROM WS-HEADER-1 AFTER ADVANCING 1 LINE
+           END-WRITE
+       END-IF
+           MOVE 'N' TO WS-CL-RECORD-FOUND.
        PERFORM READ-CLIENT-NEXT-PARA.
 
       *******************************
@@ -230,4 +305,5 @@
 
       *******************************
        END-PROGRAM-PARA.
-       STOP RUN.
+       STOP RUN
+       EXIT PROGRAM.
